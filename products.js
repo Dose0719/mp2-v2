e54f7f3607af8da6636cb124e99ed9c1909af214
Dose0719 <gabrieljmataya@gmail.com>
2023-09-03 05:11:27 +0800

    first commit

---
+++ products.js
@@ -0,0 +1,57 @@
+const products = [
+    {
+      id: 0,
+      name: "NIKE",
+      price: 80,
+      instock: 100,
+      description:
+        "Nike Phantom Venom Academy FG",
+      imgSrc: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/whegph8z9ornhxklc8rp/phantom-venom-academy-fg-firm-ground-soccer-cleat-6JVNll.jpg",
+    },
+    {
+      id: 1,
+      name: "NIKE",
+      price: 90,
+      instock: 43,
+      description:
+        "Nike Air Force 1",
+      imgSrc: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/178b2a46-3ee4-492b-882e-f71efdd53a36/air-force-1-big-kids-shoe-2zqp8D.jpg",
+    },
+    {
+      id: 2,
+      name: "NIKE",
+      price: 100,
+      instock: 10,
+      description:
+        "Nike Air Max 90",
+      imgSrc: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8439f823-86cf-4086-81d2-4f9ff9a66866/air-max-90-big-kids-shoe-1wzwJM.jpg",
+    },
+    {
+      id: 3,
+      name: "NIKE",
+      price: 110,
+      instock: 5,
+      description:
+        "Nike Air Max 90 LTR",
+      imgSrc: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-620aeb37-1b28-44b0-9b14-5572f8cbc948/air-max-90-ltr-big-kids-shoe-hdNLQ5.jpg",
+    },
+    {
+      id: 4,
+      name: "NIKE",
+      price: 110,
+      instock: 4,
+      description:
+        "Nike Joyride Dual Run",
+      imgSrc: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/33888130-0320-41a1-ba53-a026decd8aa2/joyride-dual-run-big-kids-running-shoe-1HDJF8.jpg",
+    },
+    {
+      id: 5,
+      name: "NIKE",
+      price: 39.99,
+      instock: 40,
+      description:
+        "Nike Renew Run",
+      imgSrc: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-73e54c0b-11a6-478b-9f90-bd97fcde872d/renew-run-big-kids-running-shoe-5Bpz93.jpg",
+    },
+  ];
+  
